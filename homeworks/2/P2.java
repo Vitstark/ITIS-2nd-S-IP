d4aabab313f8ea4e3cbd905b9761cbc51c878d0d
Vitalii Chekushkin <89994306+Vitstark@users.noreply.github.com>
2022-03-22 12:06:04 +0300

    changed method

--- homeworks/2/P2.java
+++ homeworks/2/P2.java
@@ -5,11 +5,11 @@ import java.util.List;
 public class P2 {
 
     public static <T> int task(List<T> list, Function<T, Integer> f) {
-        return list.stream()
+        return (int) list.stream()
                 .map(f)
                 .filter(x -> x % 2 == 1)
                 .mapToInt(x -> x)
-                .sum();
+                .count();
     }
 
 }
