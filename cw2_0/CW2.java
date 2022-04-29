453e7dc3e55aae4acdd5539aa65dfe0bc6250857
Mansur Ziiatdinov <gltronred@gmail.com>
2022-04-29 15:51:05 +0300

    CW2 - var 0

---
+++ cw2_0/CW2.java
@@ -0,0 +1,34 @@
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class CW2 {
+    // 1
+    // Тесты для splitLast2
+    // Можно добавить ещё методов-тестов, если нужно
+    @Test
+    public void splitTest() {
+        assertEquals(4,2+2);
+    }
+
+    // 2
+    // Скачать файлы
+    public static void task2() {
+    }
+
+    // 3
+    // Посчитать суммарное место
+    public static void task3() {
+    }
+
+    public static List<Integer> splitLast(String s, int n) {
+        return new LinkedList<>();
+    }
+    public static void main(String[] args) {
+    }
+}
+
+class FormatException extends Exception {}
